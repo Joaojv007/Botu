b85ed2179124678b13f1e0cb7bdc34b7fe929590
Joao vitor <victor.flop@hotmail.com>
2024-02-03 19:25:49 -0300

    Implementação hangfire com mysql

---
+++ Infra/Hangfire/Jobs/SigaNotasJob.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infra.Hangfire.Jobs
+{
+    public class SigaNotasJob
+    {
+        public async Task<GenericCommandResult> Run()
+        {
+            return new GenericCommandResult(true, "teste", "teste");
+        }
+    }
+}
