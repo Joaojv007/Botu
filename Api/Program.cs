c7d6b889e9bcefb487f5c0798c295ec6068e49d0
JV <victor.flop@gmail.com>
2025-07-20 19:50:27 -0300

    teste

--- Api/Program.cs
+++ Api/Program.cs
@@ -21,7 +21,7 @@ using Infra.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container..
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
